013669c52ea0db683914508b3b097a37c623f618
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-03-08 14:24:41 -0300

    Use cgal@4 formula

--- configure.sh
+++ configure.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 echo 'export WM_COMPILER=Clang' >> etc/prefs.sh
-echo 'export CPATH=$(brew --prefix libomp)/include' >> etc/prefs.sh
-echo 'export LIBRARY_PATH=$(brew --prefix libomp)/lib' >> etc/prefs.sh
+echo 'export CPATH=$(brew --prefix libomp)/include:$(brew --prefix gmp)/include:$(brew --prefix mpfr)/include' >> etc/prefs.sh
+echo 'export LIBRARY_PATH=$(brew --prefix libomp)/lib:$(brew --prefix gmp)/lib:$(brew --prefix mpfr)/lib' >> etc/prefs.sh
 bin/tools/foamConfigurePaths \
     -adios-path '$(brew --prefix adios2)' \
     -boost-path '$(brew --prefix boost)' \
+    -cgal-path '$(brew --prefix cgal\@4)' \
     -cmake-path '$(brew --prefix cmake)' \
     -fftw-path '$(brew --prefix fftw)' \
     -kahip-path '$(brew --prefix kahip)' \
