40b684dc5f20c23d31dd5b9e15a6e6dfc7ef74ed
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-12-31 01:05:04 -0300

    Update scripts

---
+++ scripts/macho.py
@@ -0,0 +1,26 @@
+"""
+Utility functions for working with macOS Mach-O files.
+"""
+import subprocess
+import platform
+import os
+
+from pathlib import Path
+
+def _codesign(file):
+    subprocess.run(["codesign", "--sign", "-", "--force", "--preserve-metadata=entitlements,requirements,flags,runtime", file], check=True)
+
+def get_install_names(file):
+    otool_stdout = subprocess.run(["otool", "-L", file], stdout=subprocess.PIPE, check=True).stdout.decode()
+    install_names = [Path(line.rpartition(" (compatibility version ")[0].strip()) for line in otool_stdout.splitlines()[1:]]
+    return install_names
+
+def change_install_name(file, old_install_name, new_install_name, *, relative=False):
+    file = Path(file)
+    if relative:
+        new_install_name = Path(new_install_name)
+        assert new_install_name.is_absolute()
+        new_install_name = Path("@loader_path") / os.path.relpath(new_install_name, start=file.parent)
+    subprocess.run(["install_name_tool", "-change", old_install_name, new_install_name, file], check=True)
+    if platform.machine() == "arm64":
+        _codesign(file)
--- scripts/relativize_install_names.py
+++ scripts/relativize_install_names.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
+"""
+Replace absolute references to dylibs in OpenFOAM binaries with relative references.
+"""
 
-import os
 import subprocess
 
 from pathlib import Path
 
+import macho
+
 def relativize_install_names(file, lib_dirs):
-    otool_stdout = subprocess.run(["otool", "-L", file], stdout=subprocess.PIPE, check=True).stdout.decode()
-    install_names = [Path(line.split(" (compatibility version ")[0].strip()) for line in otool_stdout.splitlines()[1:]]
-    for install_name in install_names:
+    for install_name in macho.get_install_names(file):
         if install_name.is_absolute():
             for lib_dir,new_lib_dir in lib_dirs.items():
                 lib_dir = lib_dir.absolute()
                 if install_name.is_relative_to(lib_dir):
                     new_install_name = new_lib_dir.absolute() / install_name.relative_to(lib_dir)
-                    relative_install_name = Path("@loader_path") / os.path.relpath(new_install_name, start=file.parent)
-                    subprocess.run(["install_name_tool", "-change", install_name, relative_install_name, file])
+                    macho.change_install_name(file, install_name, new_install_name, relative=True)
                     break
 
 # Replace references to dependencies
